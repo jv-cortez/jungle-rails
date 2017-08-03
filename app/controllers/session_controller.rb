c9416f556b14c27de46c86dd751cc17599d4744c
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-03 22:54:51 +0000

    added new and destroy methods

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,15 @@
 class SessionController < ApplicationController
+  
   def new
+    @user_session = User.new
   end
 
-  def create
-  end
+  # def create
+  # end
 
   def destroy
+    user = User.find params[:id]
+    product.destroy
+    redirect_to [:admin, :products], notice: 'Product deleted!'
   end
 end
