e7abc5e9d9cf93d0c44d7cfe0885420627fa8f07
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-03 00:20:16 +0000

    removed white space

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,8 +5,7 @@ class CartsController < ApplicationController
 
   def add_item
     product_id = params[:product_id].to_s
-    logger.debug "what am i doing: #{product_id.inspect}"
-    logger.info
+  
     item = cart[product_id] || { "quantity" => 0 }
     item["quantity"] += 1
     cart[product_id] = item
