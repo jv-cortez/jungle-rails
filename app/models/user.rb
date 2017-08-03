d7833db980b80d8f8f46447e0f95f0e7b08fdcc7
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-03 22:10:32 +0000

    made user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
-  validates: :name, :email, :password_digest, presence: true
-  #validates: :password_digest, presence: true
+  has_secure_password
+  validates :first_name, 
+            :last_name, presence: true
+  validates :email, presence: true, 
+                    uniqueness: true
+  validates :password, presence: true 
+  #custom validation for password wtih p confirmation
+
 end
