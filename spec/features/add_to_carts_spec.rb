b733acf747ac782e8a40c503e475ff58dd8a5458
JV Cortez <juanvictor.cortez@gmail.com>
2017-08-08 13:53:20 -0700

    created test to see if clicking the add button on a product on the home page would add it to the cart

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+     @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+    end
+  end
+
+  scenario "Allow the user to add an item to their cart" do
+    visit '/'
+
+    first('footer.actions').find_link('Add').click
+    # save_screenshot 'add_to_car.png'
+    puts page.html 
+    expect(page).to have_text('My Cart (1)')
+    end
+end
